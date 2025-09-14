bb7ab3fdfc3d7c1cd869f868e18988057e5b3508
Ziad Ismael <ziad.ismael@icloud.com>
2025-09-14 16:31:03 +0300

    feat: implemented the connectDB() with the DB URI

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { PORT } from './config/config.js';
+import connectDB from './database/mongodb.js';
 
 const app = express();
 
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
+    await connectDB();
 });
 
 export default app;
